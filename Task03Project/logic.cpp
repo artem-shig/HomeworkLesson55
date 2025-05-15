18c3365b6097def3bfee505c32f162729253ef0e
artem <user@WIN-S072TAIHVAH>
2025-05-15 23:27:55 +0300

    Improved Task03

--- Task03Project/logic.cpp
+++ Task03Project/logic.cpp
@@ -1,6 +1,24 @@
 #include "logic.h"
 
+//double function(double x, unsigned int n) {
+//
+//    return n == 0 ? 1 : (x / n) * function(x, n - 1);
+//}
+
 double function(double x, unsigned int n) {
 
-    return (n == 0 ? 1 : (x / n) * function(x, n - 1));
+	int factorial = 1;
+	double xPower = 1.0;
+
+	for (int i = 1; i <= n; i++)
+	{
+		factorial *= i;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		xPower *= x;
+	}
+
+    return n == 0 ? 1 : xPower / factorial;
 }
--- Task03Project/logic.h
+++ Task03Project/logic.h
@@ -1,4 +1,5 @@
 #include <iostream>
 using namespace std;
 
+//double function(double x, unsigned int n);
 double function(double x, unsigned int n);
